6fc618b79aee5d210f347648beaadc20e78c5608
RVxLab <code@rvx.works>
2024-07-19 18:23:04 +1000

    Proper testcase setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace RVxLab\Analytics\Tests;
 
+use Illuminate\Config\Repository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use RVxLab\Analytics\AnalyticsServiceProvider;
 use RVxLab\Analytics\Middleware\RecordPageView;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+
     /**
      * Get package providers.
      *
@@ -31,6 +35,38 @@ abstract class TestCase extends BaseTestCase
      */
     protected function defineRoutes($router)
     {
-        $router->get('/')->middleware(RecordPageView::class);
+        $router->get('/', function () {
+            return 'Hello, world!';
+        })->middleware(RecordPageView::class);
+    }
+
+    /**
+    * Define environment setup.
+    *
+    * @param  \Illuminate\Foundation\Application  $app
+    * @return void
+    */
+    protected function defineEnvironment($app)
+    {
+        // Setup default database to use sqlite :memory:
+        tap($app['config'], function (Repository $config) {
+            $config->set('database.default', 'testbench');
+            $config->set('database.connections.testbench', [
+                'driver'   => 'sqlite',
+                'database' => ':memory:',
+                'prefix'   => '',
+            ]);
+        });
+    }
+
+    /**
+       * Get the application timezone.
+       *
+       * @param  \Illuminate\Foundation\Application  $app
+       * @return string|null
+       */
+    protected function getApplicationTimezone($app)
+    {
+        return 'UTC';
     }
 }
