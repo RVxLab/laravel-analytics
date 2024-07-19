c56e1bae607339cc65428793eb8dc31eea6e8c77
RVxLab <code@rvx.works>
2024-07-19 17:32:11 +1000

    Create AnalyticsEvent model

--- config/config.php
+++ config/config.php
@@ -3,10 +3,19 @@
 declare(strict_types=1);
 
 return [
-    /*
-     * The connection to use for analytics
-     *
-     * By default, the default connection of the application is used.
-     */
-    'db_connection' => env('ANALYTICS_DB_CONNECTION', config('database.default')),
+    'database' => [
+        /*
+         * The connection to use for analytics
+         *
+         * By default, the default connection of the application is used.
+         */
+        'connection' => env('ANALYTICS_DB_CONNECTION'),
+
+        /*
+         * The names of the tables
+         */
+        'table_names' => [
+
+        ],
+    ],
 ];
---
+++ src/Models/AnalyticsEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RVxLab\Analytics\Models;
+
+use Illuminate\Database\Eloquent;
+
+/**
+ * @property string $type
+ * @property array<array-key, mixed> $data
+ * @property ?string $ip_address
+ * @property ?string $user_agent
+ */
+class AnalyticsEvent extends Eloquent\Model
+{
+    use Eloquent\Concerns\HasUuids;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function getConnectionName(): ?string
+    {
+        /** @var ?string */
+        $connection = config('analytics.database.connection', parent::getConnectionName());
+
+        return $connection;
+    }
+
+    public function getTable(): ?string
+    {
+        /** @var ?string */
+        $table = config('analytics.database.table_names.analytics_events');
+
+        return $table;
+    }
+
+    /** @return array{ data: 'json' } */
+    public function casts(): array
+    {
+        return [
+            'data' => 'json',
+        ];
+    }
+}
