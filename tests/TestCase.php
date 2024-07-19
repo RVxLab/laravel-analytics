903059fdbae8ca20e066a7790b558a06aaf3ec25
RVxLab <code@rvx.works>
2024-07-19 18:03:25 +1000

    Add correct testing namespace

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,35 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace RVxLab\Analytics\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use RVxLab\Analytics\AnalyticsServiceProvider;
+use RVxLab\Analytics\Middleware\RecordPageView;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            AnalyticsServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function defineRoutes($router)
+    {
+        $router->get('/')->middleware(RecordPageView::class);
+    }
 }
